a43e4489e43887aa8bc5e7b46e1a7a06a5f92c25
Jose Ferrao <up201805386@up.pt>
2021-12-23 00:30:23 +0000

    Vehicle updates

--- tp3/elements/MyVehicle.js
+++ tp3/elements/MyVehicle.js
@@ -29,14 +29,16 @@ export class MyVehicle extends CGFobject {
         this.keyRight = false;
 
         this.location = new vec3.fromValues(0, 0, 0);
-        this.velocity = 1;
-        this.velocityAtriction = 0.2;
-        this.velocityDelta = 0.5;
+        this.velocity = 0;
+        this.velocityAtriction = 0.9;
+        this.velocityDelta = 0.2;
+        this.velocityMax = 2;
         this.direction = 0;
         this.lastDirection = 0;
         this.steeringAngle = 0;
         this.steeringDelta = 1;
         this.steeringAngleMax = 15;
+        this.steeringAtriction = 0.8;
     }
 
     display() {
@@ -53,6 +55,7 @@ export class MyVehicle extends CGFobject {
 
         // FL Wheel
         this.scene.pushMatrix();
+        this.scene.rotate(this.steeringAngle * Math.PI / 180, 0, 1, 0);
         this.scene.translate(0.65,0,2);
         this.scene.scale(0.2, 0.2, 0.2)
         this.scene.translate(1, 0, 0)
@@ -62,6 +65,7 @@ export class MyVehicle extends CGFobject {
 
         // FR Wheel
         this.scene.pushMatrix();
+        this.scene.rotate(this.steeringAngle * Math.PI / 180, 0, 1, 0);
         this.scene.translate(-0.65,0,2);
         this.scene.scale(0.2, 0.2, 0.2)
         this.scene.translate(-1, 0, 0)
@@ -119,7 +123,7 @@ export class MyVehicle extends CGFobject {
             else {
                 //wheels going back to place when keys aren´t being pressed
                 if (!this.keyLeft && !this.keyRight) {
-                    if (Math.abs(this.steeringAngle) > 0.001) this.steeringAngle = this.steeringAngle * 0.8;
+                    if (Math.abs(this.steeringAngle) > 0.001) this.steeringAngle = this.steeringAngle * this.steeringAtriction;
                     if (Math.abs(this.steeringAngle) < 0.001) this.steeringAngle = 0;
                 }
             }
@@ -128,21 +132,21 @@ export class MyVehicle extends CGFobject {
         //forward
         if (this.keyForward && !this.keyBackward) {
             this.velocity = this.velocity + this.velocityDelta;
-            if (this.velocity > 1) this.velocity = 1;
+            if (this.velocity > this.velocityMax) this.velocity = this.velocityMax;
             direction = 1;
         } else
             //backward
             if (!this.keyForward && this.keyBackward) {
                 this.velocity = this.velocity - this.velocityDelta;
-                if (this.velocity < -1) this.velocity = -1;
+                if (this.velocity < -this.velocityMax) this.velocity = -this.velocityMax;
                 direction = -1;
             }
 
         //not going forward or backward
-        if (direction == 0) {
+        if (!this.keyForward && !this.keyBackward) {
             if (Math.abs(this.velocity) > 0) {
                 this.velocity = this.velocity * this.velocityAtriction;
-                if (Math.abs(this.velocity) < 0.005) this.velocity = 0;
+                if (Math.abs(this.velocity) < 0.001) this.velocity = 0;
             }
         }
 
@@ -153,7 +157,7 @@ export class MyVehicle extends CGFobject {
         var deltaDirection = this.direction - this.lastDirection;
         this.lastDirection = this.direction;
 
-        // compute vehicle location based on crrent location, direction and velocity
+        // compute vehicle location based on current location, direction and velocity
         this.location[0] += (Math.sin(this.direction) * this.velocity);
         this.location[1] = 0;
         this.location[2] += (Math.cos(this.direction) * this.velocity);
--- tp3/elements/VehicleBody.js
+++ tp3/elements/VehicleBody.js
@@ -2,7 +2,9 @@ import { CGFobject, CGFappearance, CGFnurbsObject, CGFnurbsSurface } from "../..
 import { MyCylinder } from "../primitives/MyCylinder.js";
 import { MyPlane } from "../primitives/MyPlane.js";
 import { MyRectangle } from "../primitives/MyRectangle.js";
-import { MySphere} from "../primitives/MySphere.js";
+import { MySphere } from "../primitives/MySphere.js";
+import { MyPatch } from "../primitives/MyPatch.js";
+import { MyTriangle } from "../primitives/MyTriangle.js";
 
 
 
@@ -24,6 +26,14 @@ export class VehicleBody extends CGFobject {
         this.body2Tex.loadTexture('scenes/images/fwcSponsor.png');
         this.body2Tex.setTextureWrap('REPEAT', 'REPEAT');
 
+        // this.rectRWTex = new CGFappearance(this.scene);
+        // this.rectRWTex.setAmbient(1, 1, 1, 1);
+        // this.rectRWTex.setDiffuse(1, 1, 1, 1);
+        // this.rectRWTex.setSpecular(1, 1, 1, 1);
+        // this.rectRWTex.setShininess(100.0);
+        // this.rectRWTex.loadTexture('scenes/images/.jpeg');
+        // this.rectRWTex.setTextureWrap('REPEAT', 'REPEAT');
+
         this.frontwingTex = new CGFappearance(this.scene);
         this.frontwingTex.setAmbient(1, 1, 1, 1);
         this.frontwingTex.setDiffuse(1, 1, 1, 1);
@@ -31,6 +41,22 @@ export class VehicleBody extends CGFobject {
         this.frontwingTex.setShininess(100.0);
         this.frontwingTex.loadTexture('scenes/images/frontwingSponsor.jpeg');
         this.frontwingTex.setTextureWrap('REPEAT', 'REPEAT');
+
+        // this.rearwingTex = new CGFappearance(this.scene);
+        // this.rearwingTex.setAmbient(1, 1, 1, 1);
+        // this.rearwingTex.setDiffuse(1, 1, 1, 1);
+        // this.rearwingTex.setSpecular(1, 1, 1, 1);
+        // this.rearwingTex.setShininess(100.0);
+        // this.rearwingTex.loadTexture('scenes/images/.jpeg');
+        // this.rearwingTex.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.redTex = new CGFappearance(this.scene);
+        this.redTex.setAmbient(1, 1, 1, 1);
+        this.redTex.setDiffuse(1, 1, 1, 1);
+        this.redTex.setSpecular(1, 1, 1, 1);
+        this.redTex.setShininess(100.0);
+        this.redTex.loadTexture('scenes/images/red.png');
+        this.redTex.setTextureWrap('REPEAT', 'REPEAT');
 
         this.sideWingTex = new CGFappearance(this.scene);
         this.sideWingTex.setAmbient(1, 1, 1, 1);
@@ -57,9 +83,9 @@ export class VehicleBody extends CGFobject {
         this.floorTex.setTextureWrap('REPEAT', 'REPEAT');
 
         this.helmetTex = new CGFappearance(this.scene);
-        this.helmetTex.setAmbient(1, 1, 1, 1);
-        this.helmetTex.setDiffuse(1, 1, 1, 1);
-        this.helmetTex.setSpecular(1, 1, 1, 1);
+        this.helmetTex.setAmbient(0, 0, 0, 1);
+        this.helmetTex.setDiffuse(0, 0, 0, 1);
+        this.helmetTex.setSpecular(0, 0, 0, 1);
         this.helmetTex.setShininess(100.0);
         this.helmetTex.loadTexture('scenes/images/helmet.png');
         this.helmetTex.setTextureWrap('REPEAT', 'REPEAT');
@@ -71,24 +97,117 @@ export class VehicleBody extends CGFobject {
         this.wing = new MyPlane(scene, "wing", 50, 50);
         this.sideWing = new MyPlane(scene, "sideWing", 50, 50);
         this.floor = new MyRectangle(scene, "floor", 0, 2.2, 0, 2.85);
+        this.fwrectangle = new MyRectangle(scene, "frontwingRect", 0, 0.43, 0, 0.29);
         this.wheelSuport = new MyCylinder(scene, "suport", 0.7, 0.045, 0.045, 70, 4);
         this.base = new MyCylinder(scene, "base", 1.1, 1.5, 2, 3, 70);
-        this.helmet = new MySphere(scene,"helmet",0.3,40,50);
+        this.helmet = new MySphere(scene, "helmet", 0.3, 40, 50);
 
+        // let controlpoints = [
+        //     [-2, 0, 0],
+        //     [-1, 0, -1.33],
+        //     [1, 0, -1.33],
+        //     [2, 0, 0],
+        //     [-2, 0, 0],
+        //     [-0.5, 1.1547005383792515, -0.6666666666666667],
+        //     [0.5, 1.1547005383792515, -0.6666666666666667],
+        //     [0.5, 1, 0],
+        //     [-0.5, 1, 0],
+        //     [-0.5, 1.1547005383792515, 0.6666666666666663],
+        //     [0.5, 1.1547005383792515, 0.6666666666666663],
+        //     [2, 0, 0],
+        //     [-2, 0, 0],
+        //     [-0.5, 1.6328623988631375e-16, 1.3333333333333333],
+        //     [0.5, 1.6328623988631375e-16, 1.3333333333333333],
+        //     [2, 0, 0]
+        // ]
+
+        let controlpoints = [
+            [0, 0, -0.1],
+            [0, 1, -0.1],
+            [0.333, 0, -0.222],
+            [0.666, 1, -0.222],
+            [1, 0, 0.333],
+            [1, 1, 0.333],
+        ]
+
+        this.rearwing = new MyPatch(scene, "rearwing", 3, 2, 10, 10, controlpoints);
+        this.rectRW = new MyCylinder(scene, "sideRW", 0.6, 0.2, 0.2, 2, 1);
+        this.supportRW = new MyTriangle(scene, "supportRW", 0, 0, 0, 1, 0, 0, 0.5, 0.75, 0);
     }
 
     display() {
         this.scene.pushMatrix();
 
+        // Right side Rear Wing Support
+        this.scene.pushMatrix();
+        // this.floorTex.apply();
+        this.scene.translate(-0.8, 1.8, -4.6);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
+        this.supportRW.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        // this.floorTex.apply();
+        this.scene.translate(-0.8, 0.8, -4.6);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.supportRW.display();
+        this.scene.popMatrix();
+
+        // Left side Rear Wing Support
+        this.scene.pushMatrix();
+        // this.floorTex.apply();
+        this.scene.translate(0.8, 1.8, -4.6);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
+        this.supportRW.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        // this.floorTex.apply();
+        this.scene.translate(0.8, 0.8, -4.6);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.supportRW.display();
+        this.scene.popMatrix();
+
+        // Left side rect Rear Wing
+        this.scene.pushMatrix();
+        // this.rectRWTex.apply();
+        this.scene.scale(1, 1, 1);
+        this.scene.translate(0.6, 0.8, -4.6);
+        this.scene.rotate(Math.PI / 4, 1, 0, 0);
+        this.rectRW.display();
+        this.scene.popMatrix();
+
+        // Right side rect Rear Wing
+        this.scene.pushMatrix();
+        // this.rectRWTex.apply();
+        this.scene.scale(1, 1, 1);
+        this.scene.translate(-0.6, 0.8, -4.6);
+        this.scene.rotate(Math.PI / 4, 1, 0, 0);
+        this.rectRW.display();
+        this.scene.popMatrix();
+
+        // Rear Wing
+        this.scene.pushMatrix();
+        // this.rearwingTex.apply();
+        this.scene.translate(-0.8, 1.76, -4.45);
+        this.scene.rotate(-Math.PI / 2, 0, 0, 1);
+        this.scene.scale(0.6, 1.6, 1);
+        this.rearwing.display();
+        this.scene.popMatrix();
+
         // Helmet
         this.scene.pushMatrix();
-        // this.helmetTex.apply();
-        this.scene.translate(0,5,0);
-        this.scene.scale(1,1,1.1);
+        this.helmetTex.apply();
+        this.scene.translate(0, 1.1, -1);
+        this.scene.scale(1, 1, 1.1);
         this.helmet.display();
         this.scene.popMatrix();
 
-        // Left Wheel Connection 1
+        // Left Front Wheel Connection 1
         this.scene.pushMatrix();
         this.floorTex.apply();
         this.scene.scale(1.2, 1.2, 1.2);
@@ -98,7 +217,7 @@ export class VehicleBody extends CGFobject {
         this.wheelSuport.display();
         this.scene.popMatrix();
 
-        // Left Wheel Connection 2
+        // Left Front Wheel Connection 2
         this.scene.pushMatrix();
         this.floorTex.apply();
         this.scene.translate(0.2, 0.4, 2);
@@ -106,17 +225,17 @@ export class VehicleBody extends CGFobject {
         this.wheelSuport.display();
         this.scene.popMatrix();
 
-        // Left Wheel Connection 3
+        // Left Front Wheel Connection 3
         this.scene.pushMatrix();
         this.floorTex.apply();
         this.scene.scale(1.22, 1.22, 1.22);
         this.scene.translate(0.12, 0.35, 1.97);
-        this.scene.rotate(2* Math.PI / 3, 0, 1, 0);
+        this.scene.rotate(2 * Math.PI / 3, 0, 1, 0);
         this.scene.rotate(Math.PI / 90, 1, 0, 0);
         this.wheelSuport.display();
         this.scene.popMatrix();
 
-        // Right Wheel Connection 1
+        // Right Front Wheel Connection 1
         this.scene.pushMatrix();
         this.floorTex.apply();
         this.scene.scale(1.2, 1.2, 1.2);
@@ -126,7 +245,7 @@ export class VehicleBody extends CGFobject {
         this.wheelSuport.display();
         this.scene.popMatrix();
 
-        // Right Wheel Connection 2
+        // Right Front Wheel Connection 2
         this.scene.pushMatrix();
         this.floorTex.apply();
         this.scene.translate(-0.9, 0.4, 2);
@@ -134,7 +253,7 @@ export class VehicleBody extends CGFobject {
         this.wheelSuport.display();
         this.scene.popMatrix();
 
-        // Right Wheel Connection 3
+        // Right Front Wheel Connection 3
         this.scene.pushMatrix();
         this.floorTex.apply();
         this.scene.scale(1.22, 1.22, 1.22);
@@ -144,6 +263,77 @@ export class VehicleBody extends CGFobject {
         this.wheelSuport.display();
         this.scene.popMatrix();
 
+        // Left Back Wheel Connection 1
+        this.scene.pushMatrix();
+        this.floorTex.apply();
+        this.scene.scale(1.2, 1.2, 1.2);
+        this.scene.translate(0.15, 0.43, -3.65);
+        this.scene.rotate(Math.PI / 2.4, 0, 1, 0);
+        this.scene.rotate(Math.PI / 20, 1, 0, 0);
+        this.wheelSuport.display();
+        this.scene.popMatrix();
+
+        // Left Back Wheel Connection 2
+        this.scene.pushMatrix();
+        this.floorTex.apply();
+        this.scene.translate(0.2, 0.4, -4.2);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.wheelSuport.display();
+        this.scene.popMatrix();
+
+        // Left Back Wheel Connection 3
+        this.scene.pushMatrix();
+        this.floorTex.apply();
+        this.scene.scale(1.22, 1.22, 1.22);
+        this.scene.translate(0.12, 0.35, -3.11);
+        this.scene.rotate(2 * Math.PI / 3, 0, 1, 0);
+        this.scene.rotate(Math.PI / 90, 1, 0, 0);
+        this.wheelSuport.display();
+        this.scene.popMatrix();
+
+        // Right Back Wheel Connection 1
+        this.scene.pushMatrix();
+        this.floorTex.apply();
+        this.scene.scale(1.2, 1.2, 1.2);
+        this.scene.translate(-0.15, 0.43, -3.65);
+        this.scene.rotate(-Math.PI / 2.4, 0, 1, 0);
+        this.scene.rotate(Math.PI / 20, 1, 0, 0);
+        this.wheelSuport.display();
+        this.scene.popMatrix();
+
+        // Right Back Wheel Connection 2
+        this.scene.pushMatrix();
+        this.floorTex.apply();
+        this.scene.translate(-0.9, 0.4, -4.2);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.wheelSuport.display();
+        this.scene.popMatrix();
+
+        // Right Back Wheel Connection 3
+        this.scene.pushMatrix();
+        this.floorTex.apply();
+        this.scene.scale(1.22, 1.22, 1.22);
+        this.scene.translate(-0.12, 0.35, -3.11);
+        this.scene.rotate(-2 * Math.PI / 3, 0, 1, 0);
+        this.scene.rotate(Math.PI / 90, 1, 0, 0);
+        this.wheelSuport.display();
+        this.scene.popMatrix();
+
+        // Floor
+        this.scene.pushMatrix();
+        this.floorTex.apply();
+        this.scene.translate(-1.1, 0.05, -0.9);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.floor.display();
+        this.scene.popMatrix();
+
+        // Inside Floor
+        this.scene.pushMatrix();
+        this.floorTex.apply();
+        this.scene.translate(-1.1, 0.05, -3.8);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.floor.display();
+        this.scene.popMatrix();
 
         // Front Wing Connection 
         this.scene.pushMatrix();
@@ -156,28 +346,21 @@ export class VehicleBody extends CGFobject {
         this.frontConn.display();
         this.scene.popMatrix();
 
-        // Floor
+        // Front Wing Rectangle
         this.scene.pushMatrix();
-        this.floorTex.apply();
-        this.scene.translate(-1.1, 0.05, -0.9);
-        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
-        this.floor.display();
+        this.redTex.apply();
+        this.scene.scale(0.75, 0.7, 0.75);
+        this.scene.translate(-0.22, 0.3, 4.04);
+        this.scene.rotate(Math.PI / 26, 1, 0, 0);
+        this.fwrectangle.display();
         this.scene.popMatrix();
-
-         // Inside Floor
-         this.scene.pushMatrix();
-         this.floorTex.apply();
-         this.scene.translate(-1.1, 0.05, -3.8);
-         this.scene.rotate(Math.PI / 2, 1, 0, 0);
-         this.floor.display();
-         this.scene.popMatrix();
 
         //Right sideFrontWing
         this.scene.pushMatrix();
         this.sideWingTex.apply();
         this.scene.translate(-1.5, 0.20, 3.2);
         this.scene.scale(0.7, 0.25, 0.6);
-        this.scene.rotate(-Math.PI / 2, 0, 1,0);
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
         this.sideWing.display();
         this.scene.popMatrix();
 
@@ -186,7 +369,7 @@ export class VehicleBody extends CGFobject {
         this.sideWingTex.apply();
         this.scene.translate(1.5, 0.20, 3.2);
         this.scene.scale(0.7, 0.25, 0.6);
-        this.scene.rotate(Math.PI / 2, 0, 1,0);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
         this.sideWing.display();
         this.scene.popMatrix();
 
@@ -195,7 +378,7 @@ export class VehicleBody extends CGFobject {
         this.sideWingTex.apply();
         this.scene.translate(-1.5, 0.20, 3.2);
         this.scene.scale(0.7, 0.25, 0.6);
-        this.scene.rotate(Math.PI / 2, 0, 1,0);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
         this.sideWing.display();
         this.scene.popMatrix();
 
@@ -204,10 +387,10 @@ export class VehicleBody extends CGFobject {
         this.sideWingTex.apply();
         this.scene.translate(1.5, 0.20, 3.2);
         this.scene.scale(0.7, 0.25, 0.6);
-        this.scene.rotate(-Math.PI / 2, 0, 1,0);
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
         this.sideWing.display();
         this.scene.popMatrix();
-        
+
         //Frontwing
         this.scene.pushMatrix();
         this.frontwingTex.apply();
@@ -246,13 +429,13 @@ export class VehicleBody extends CGFobject {
 
         // Base
         this.scene.pushMatrix();
-        this.scene.scale(0.7,1,1.2);
-        this.scene.translate(0,0, -2);
+        this.scene.scale(0.7, 1, 1.2);
+        this.scene.translate(0, 0, -2);
         this.scene.rotate(-Math.PI / 6, 0, 1, 0);
         this.scene.rotate(-Math.PI / 2, 1, 0, 0);
         this.base.display();
         this.scene.popMatrix();
-        
+
         this.scene.popMatrix();
 
     }

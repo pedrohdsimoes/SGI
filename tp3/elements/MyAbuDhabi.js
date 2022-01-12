570a96f09443b4644adf09eb9f6e25148c487956
pedrohdsimoes <63207385+pedrohdsimoes@users.noreply.github.com>
2022-01-12 11:34:50 +0000

    ad view

--- tp3/elements/MyAbuDhabi.js
+++ tp3/elements/MyAbuDhabi.js
@@ -6,9 +6,14 @@ import {
     CGFappearance,
     CGFobject
 } from '../../lib/CGF.js';
+import { MyQuad } from '../../TextExample/MyQuad.js';
+import { MyCylinder } from '../primitives/MyCylinder.js';
 import {
     MyPlane
 } from '../primitives/MyPlane.js';
+import { MyRectangle } from '../primitives/MyRectangle.js';
+import { MyTree } from './MyTree.js';
+
 
 
 /**
@@ -23,25 +28,100 @@ export class MyAbuDhabi extends CGFobject {
 
         this.scene = scene;
 
-        this.trackText = new CGFappearance(this.scene);
-        this.trackText.setAmbient(1, 1, 1, 1);
-        this.trackText.setDiffuse(1, 1, 1, 1);
-        this.trackText.setSpecular(1, 1, 1, 1);
-        this.trackText.setShininess(10.0);
-        this.trackText.loadTexture('scenes/images/trackbackground.png');
-        this.trackText.setTextureWrap('REPEAT', 'REPEAT');
+        this.trackTex = new CGFappearance(this.scene);
+        this.trackTex.setAmbient(1, 1, 1, 1);
+        this.trackTex.setDiffuse(1, 1, 1, 1);
+        this.trackTex.setSpecular(1, 1, 1, 1);
+        this.trackTex.setShininess(10.0);
+        this.trackTex.loadTexture('scenes/images/trackbackground.png');
+        this.trackTex.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.toptreeTex = new CGFappearance(this.scene);
+        this.toptreeTex.setAmbient(1, 1, 1, 1);
+        this.toptreeTex.setDiffuse(1, 1, 1, 1);
+        this.toptreeTex.setSpecular(1, 1, 1, 1);
+        this.toptreeTex.setShininess(10.0);
+        this.toptreeTex.loadTexture('scenes/images/toptree.jpg');
+        this.toptreeTex.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.basetreeTex = new CGFappearance(this.scene);
+        this.basetreeTex.setAmbient(1, 1, 1, 1);
+        this.basetreeTex.setDiffuse(1, 1, 1, 1);
+        this.basetreeTex.setSpecular(1, 1, 1, 1);
+        this.basetreeTex.setShininess(10.0);
+        this.basetreeTex.loadTexture('scenes/images/basetree.jpg');
+        this.basetreeTex.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.fans = new CGFappearance(this.scene);
+        this.fans.setAmbient(1, 1, 1, 1);
+        this.fans.setDiffuse(1, 1, 1, 1);
+        this.fans.setSpecular(1, 1, 1, 1);
+        this.fans.setShininess(10.0);
+        this.fans.loadTexture('scenes/images/fans.jpeg');
+        this.fans.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.marine = new CGFappearance(this.scene);
+        this.marine.setAmbient(1, 1, 1, 1);
+        this.marine.setDiffuse(1, 1, 1, 1);
+        this.marine.setSpecular(1, 1, 1, 1);
+        this.marine.setShininess(10.0);
+        this.marine.loadTexture('scenes/images/marine.jpeg');
+        this.marine.setTextureWrap('REPEAT', 'REPEAT');
+
         this.track = new MyPlane(this.scene, "AbuDhabi", 50, 50);
+        this.tree = new MyTree(this.scene, "tree");
 
     }
 
     display() {
-        this.trackText.apply();
+        this.trackTex.apply();
 
         this.scene.pushMatrix();
         this.scene.translate(256, 0, 256);
         this.scene.scale(512, 1, 512);
         this.scene.rotate(-Math.PI / 2, 1, 0, 0);
         this.track.display();
+        this.scene.popMatrix();
+
+
+
+        this.scene.pushMatrix();
+        this.fans.apply();
+        this.scene.translate(256, 40, 0);
+        this.scene.scale(512, 80, 1);
+        this.track.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.fans.apply();
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.scene.translate(-256, 40, 0);
+        this.scene.scale(512, 80, 1);
+        this.track.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.fans.apply();
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
+        this.scene.translate(256, 40, -512);
+        this.scene.scale(512, 80, 1);
+        this.track.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.marine.apply();
+         this.scene.rotate(Math.PI , 0, 1, 0);
+        this.scene.translate(-256, 80, -512);
+        this.scene.scale(512, 160, 1);
+        this.track.display();
+        this.scene.popMatrix();
+
+
+        // _______________TREES_______________________
+
+        this.scene.pushMatrix();
+        this.scene.translate(100, 0, 50);
+        this.tree.display();
         this.scene.popMatrix();
     }
 }
---
+++ tp3/elements/MyTree.js
@@ -0,0 +1,81 @@
+import {
+    CGFscene,
+    CGFaxis,
+    CGFcamera,
+    CGFplane,
+    CGFappearance,
+    CGFobject
+} from '../../lib/CGF.js';
+import { MyQuad } from '../../TextExample/MyQuad.js';
+import { MyCylinder } from '../primitives/MyCylinder.js';
+import {
+    MyPlane
+} from '../primitives/MyPlane.js';
+import { MyRectangle } from '../primitives/MyRectangle.js';
+
+
+
+/**
+ * MyTree
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+
+export class MyTree extends CGFobject {
+    constructor(scene,id) {
+        super(scene);
+
+        this.scene = scene;
+
+        this.toptreeTex = new CGFappearance(this.scene);
+        this.toptreeTex.setAmbient(1, 1, 1, 1);
+        this.toptreeTex.setDiffuse(1, 1, 1, 1);
+        this.toptreeTex.setSpecular(1, 1, 1, 1);
+        this.toptreeTex.setShininess(10.0);
+        this.toptreeTex.loadTexture('scenes/images/toptree.jpg');
+        this.toptreeTex.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.basetreeTex = new CGFappearance(this.scene);
+        this.basetreeTex.setAmbient(1, 1, 1, 1);
+        this.basetreeTex.setDiffuse(1, 1, 1, 1);
+        this.basetreeTex.setSpecular(1, 1, 1, 1);
+        this.basetreeTex.setShininess(10.0);
+        this.basetreeTex.loadTexture('scenes/images/basetree.jpg');
+        this.basetreeTex.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.basetree = new MyCylinder(this.scene, "basetree", 6.5, 0.8, 0.8, 16, 1);
+        this.toptree = new MyCylinder(this.scene, "toptree", 7, 0, 3.3, 4, 2);
+        this.rect = new MyRectangle(this.scene, "tree", 0, 4.7, 0, 4.7);
+
+    }
+    display() {
+        this.scene.pushMatrix();
+        this.scene.scale(2, 2, 2)
+
+        this.scene.pushMatrix();
+        this.basetreeTex.apply();
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.basetree.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.toptreeTex.apply();
+        this.scene.translate(0, 6.4, 0)
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.toptree.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.toptreeTex.apply();
+        this.scene.translate(-3.4, 6.4, 0)
+        this.scene.rotate(Math.PI / 4, 0, 1, 0)
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.rect.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+    }
+
+
+
+}
